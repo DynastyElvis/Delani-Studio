1332bccc12c34ea91bda209436bc1ddb7b6ce73d
DynastyElvis <79464171+DynastyElvis@users.noreply.github.com>
2022-03-13 19:45:37 +0300

    toggle

--- js/script.js
+++ js/script.js
@@ -2,4 +2,24 @@
 
 // the beginning of javascript document for the project
 
+// toggle function a 
 
+$(document).ready(function(){
+    $("#one").click(function(){
+        $(".design").toggle();
+        $("#one").hide();
+    })
+})
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#one").toggle();
+        $(".design").hide();
+    })
+})
+
+$(document).ready(function(){
+    $("#two").click(function(){
+        $(".development").toggle();
+        $("#two").hide();
+    })
+})
